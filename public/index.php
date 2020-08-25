0907f86488cf11fdc93228d6d5d6cb416da46928
Cuento3yLlevo2 <mhermoso1993@gmail.com>
2020-08-26 00:36:11 +0200

    added user login and logout

--- public/index.php
+++ public/index.php
@@ -6,9 +6,13 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once '../vendor/autoload.php';
+
+session_start();
+
 use Illuminate\Database\Capsule\Manager as Capsule;
 // Aura.Router
 use Aura\Router\RouterContainer;
+
 
 $capsule = new Capsule;
 
@@ -39,34 +43,67 @@ $request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
 // Aura.Router
 $routerContainer = new RouterContainer();
 $map = $routerContainer->getMap();
+
+// main page mapping
 $map->get('index', '/cursophp/', [
     'controller'=> 'App\Controllers\IndexController',
     'action' => 'indexAction'
     ]);
+
+// jobs mapping 
 $map->get('addJobs', '/cursophp/jobs/add', [
+    'controller'=> 'App\Controllers\AddJobController',
+    'action' => 'getAddJobAction',
+    'auth' => true
+    ]);
+$map->post('saveJobs', '/cursophp/jobs/add', [
     'controller'=> 'App\Controllers\AddJobController',
     'action' => 'getAddJobAction'
     ]);
+
+// Projects mapping
 $map->get('addProjects', '/cursophp/projects/add', [
+    'controller'=> 'App\Controllers\AddProjectController',
+    'action' => 'getAddProjectAction',
+    'auth' => true
+    ]);
+$map->post('saveProjects', '/cursophp/projects/add', [
     'controller'=> 'App\Controllers\AddProjectController',
     'action' => 'getAddProjectAction'
     ]);
+
+// User mapping
 $map->get('addUsers', '/cursophp/users/add', [
     'controller'=> 'App\Controllers\AddUserController',
-    'action' => 'getAddUserAction'
+    'action' => 'getAddUserAction',
+    'auth' => true
     ]);
-$map->post('saveJobs', '/cursophp/jobs/add', [
-      'controller'=> 'App\Controllers\AddJobController',
-      'action' => 'getAddJobAction'
-      ]);
-$map->post('saveProjects', '/cursophp/projects/add', [
-      'controller'=> 'App\Controllers\AddProjectController',
-      'action' => 'getAddProjectAction'
-      ]);
 $map->post('saveUsers', '/cursophp/users/add', [
     'controller'=> 'App\Controllers\AddUserController',
     'action' => 'getAddUserAction'
     ]);
+
+// Login mapping
+$map->get('loginForm', '/cursophp/login', [
+    'controller'=> 'App\Controllers\AuthController',
+    'action' => 'getLogin'
+    ]);
+$map->post('auth', '/cursophp/auth', [
+    'controller'=> 'App\Controllers\AuthController',
+    'action' => 'postLogin'
+    ]);
+$map->get('logout', '/cursophp/logout', [
+    'controller'=> 'App\Controllers\AuthController',
+    'action' => 'getLogout'
+    ]);
+
+// admin mapping
+$map->get('admin', '/cursophp/admin', [
+    'controller'=> 'App\Controllers\AdminController',
+    'action' => 'getIndex',
+    'auth' => true
+    ]);
+
 
 $matcher = $routerContainer->getMatcher();
 $route = $matcher->match($request);
@@ -77,8 +114,25 @@ if (!$route) {
     $handlerData = $route->handler;
     $controllerName = $handlerData['controller'];
     $actionName = $handlerData['action'];
+    $needsAuth = $handlerData['auth'] ?? false;
+
+    $sessionUserId = $_SESSION['userId'] ?? null; 
+
+    if ($needsAuth && !$sessionUserId) {
+        $controllerName = 'App\Controllers\AuthController';
+        $actionName = 'getLogout';
+    }
+
     // instanciar una clase con una cadena y ejecuta funcion con una cadena
     $controller = new $controllerName;
     $response = $controller->$actionName($request);
+
+    foreach($response->getHeaders() as $name => $values) {
+        foreach($values as $value) {
+            header(sprintf('%s: %s', $name, $value), false);
+        }
+    }
+    http_response_code($response->getStatusCode());
+
     echo $response->getBody();
 }
